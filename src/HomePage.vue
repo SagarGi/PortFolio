0c4157810226a66f40d66dc270ceae936d17c001
sagar <sagargurung1001@gmail.com>
2023-04-01 15:11:40 +0545

    Added Header

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,8 +1,46 @@
 <template>
-  <h1>This is my portfolio</h1>
+  <div class="app" :class="modeType">
+    <HeaderComponent :modeType="modeType" @changeThemeMode="changeThemeMode"/>
+  </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      modeType: "light"
+    }
+  },
+  components: {
+    HeaderComponent
+  },
+  methods: {
+    changeThemeMode() {
+      this.modeType = this.modeType === 'light' ? 'dark' : 'light'
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto Slab";
+}
+
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #ffffff;
+}
+
+.dark {
+  background: #000;
+  color: #fff;
+}
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <h2>{{namePrefix}}<span id="suffix">{{nameSuffix}}</span></h2>
+    <nav>
+      <button class="ping-me" :class="modeType">Ping Me</button>
+      <span v-if="modeType === 'light'" class="theme-icon" @click="$emit('changeThemeMode')"><i class="fa fa-moon-o"></i></span>
+      <span v-else class="theme-icon" @click="$emit('changeThemeMode')"><i class="fa fa-sun-o"></i></span>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "HeaderComponent",
+    props: ['modeType'],
+    data() {
+      return {
+        namePrefix: "<Sagar",
+        nameSuffix: "Gi />"
+      }
+    }
+  }
+</script>
+
+<style>
+
+header {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 15vh;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  width: 100px;
+}
+
+.theme-icon:hover {
+  cursor: pointer;
+}
+
+#suffix {
+  color: #FF4539;
+}
+
+.ping-me {
+  border: none;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.light button{
+  background-color: white;
+}
+
+.dark button{
+  background-color: black;
+}
+</style>
