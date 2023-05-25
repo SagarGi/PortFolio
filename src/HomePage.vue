0673b974cdf15f234bb5cf4de39704490fbbab7d
sagargurung1001@gmail.com <sagargurung1001@gmail.com>
2023-05-25 17:14:06 +0545

    Add contact form

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,37 +1,51 @@
 <template>
   <div class="app" :class="modeType">
-    <HeaderComponent :modeType="modeType" @changeThemeMode="changeThemeMode"/>
-    <ContentComponent :modeType="modeType"/>
-    <FooterComponent :modeType="modeType"/>
+    <HeaderComponent
+      :modeType="modeType"
+      @changeThemeMode="changeThemeMode"
+      @pingMe="pingMe"
+    />
+    <ContentComponent :modeType="modeType" />
+    <FooterComponent :modeType="modeType" />
+    <ContactForm
+      v-if="showModal"
+      :mode-type="modeType"
+      :show-modal="showModal"
+    />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
-import ContentComponent from "./components/ContentComponent.vue"
-import FooterComponent from "./components/FooterComponent.vue"
+import ContentComponent from "./components/ContentComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import ContactForm from "./components/utilsComponents/ContactForm.vue";
 export default {
   name: "HomePage",
   data() {
     return {
-      modeType: "light"
-    }
+      modeType: "light",
+      showModal: false,
+    };
   },
   components: {
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    ContactForm,
   },
   methods: {
     changeThemeMode() {
-      this.modeType = this.modeType === 'light' ? 'dark' : 'light'
-    }
-  }
-}
+      this.modeType = this.modeType === "light" ? "dark" : "light";
+    },
+    pingMe() {
+      this.showModal = true;
+    },
+  },
+};
 </script>
 
 <style>
-
 * {
   margin: 0;
   padding: 0;
@@ -51,9 +65,8 @@ export default {
 
 /* Responsive layout - makes a one column-layout instead of two-column layout */
 @media (max-width: 800px) {
-    .app {
-        height: auto;
-    }
+  .app {
+    height: auto;
+  }
 }
-
 </style>
---
+++ src/components/utilsComponents/ContactForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="modal-vue">
+    <div class="overlay" v-if="showModal" @click="showModal"></div>
+    <div class="modal" v-if="showModal">
+      <button class="close" @click="showModal">x</button>
+      <h3>Title</h3>
+      <p>Description</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactForm",
+  props: ["modeType", "showModal"],
+  data() {},
+};
+</script>
+
+<style>
+.modal-vue .overlay {
+  top: 0;
+  position: absolute;
+  left: 0;
+  z-index: 9998;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-vue .modal {
+  position: relative;
+  left: -50%;
+  width: 400px;
+  z-index: 9999;
+  background-color: #fff;
+}
+</style>
